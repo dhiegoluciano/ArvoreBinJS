84b643b9ded4605553bd42585ae6dcd8ddef4994
dhiegoluciano <67329009+dhiegoluciano@users.noreply.github.com>
2020-06-24 16:44:19 -0300

    Update binary_tree.js

--- js/binary_tree.js
+++ js/binary_tree.js
@@ -5,6 +5,10 @@ class BinaryTree {
     }
 
     //exibe o menor valor da arvore
+    //Dhiego: ele verifica se o valor na variavel é nulo, caso seja ele retorna nulo
+    //caso não, ele vai descer a arvore verificando a esquerda de cada raiz, até que
+    //encontre uma raiz que a esquerda que seja nula, ai ela sera o menor valor.
+    min() {
     min() {
         let current = this.root
         if (current == null)
@@ -15,6 +19,9 @@ class BinaryTree {
     }
 
     //exibe o maior valor da arvore
+    //Dhiego: ele verifica se o valor na variavel é nulo, caso seja ele retorna nulo
+    //caso não, ele vai descer a arvore verificando a direita de cada raiz, até que
+    //encontre uma raiz que a direita seja nula, ai ela sera o maior valor.
     max() {
         let current = this.root
         if (current == null)
@@ -25,6 +32,10 @@ class BinaryTree {
     }
 
     //insere o elemento da arvores
+    //Dhiego: metodo de inserção: são chamadas recursivas para inserção, ele chama de
+    //maneira recusriva o insert nó, se ele encontrar o lugar ele retorna o nó e a posição
+    //que era nula se torna o novo elemento, se não, se for maior ou menor ele manda inserir
+    //na direita ou na esquerda.
     insert(element) {
         this.root = this.insertNode(this.root, element)
     }
@@ -40,6 +51,8 @@ class BinaryTree {
     }
 
     //executa a função callback para cada nó, em ordem
+    //Dhiego: Ele primeiro verifica se tá o node tá nulo caso esteja ele não mostra nada
+    //se não ai mostra a esquerda, mostra  a raiz e por fim a direita
     inOrderTraverse(callback) {
         this.inOrderVisitor(this.root, callback)
     }
@@ -53,6 +66,8 @@ class BinaryTree {
     }
 
     //executa a função callback para cada nó, em pré-ordem
+    //Dhiego: Ele começa da raiz, primeiro chama o conteúdo e depois chama os outros nó, 
+    //o da esquerda e depois o da direita.
     preOrderTraverse(callback) {
         this.preOrderVisitor(this.root, callback)
     }
@@ -66,6 +81,8 @@ class BinaryTree {
     }
 
     //executa a função callback para cada nó, em pós-ordem
+    //Dhiego: Nesse caso ele começa pelos nó a esquerda e a direita e depois que mostra 
+    //o conteudo.
     postOrderTraverse(callback) {
         this.postOrderVisitor(this.root, callback)
     }
@@ -85,6 +102,11 @@ class BinaryTree {
     //    3. É maior que o conteúdo?
     //       3.1 busca de direita
     //       3.2 busca na esquerda
+    //Dhiego: Ele vai verificar se o elemento é nulo, se for ele retorna nulo.
+    //Se o valor for encontrado ele retorna o valor
+    //Se não é nem nulo nem igual e se o elemento é maior que
+    //o conteudo então ele busca na direita e retorna esse valor,
+    //se caso seja menor ele vai buscar o valor na esquerda. 
 
     search(value) {
         return this.searchVisitor(this.root, value)
@@ -102,6 +124,11 @@ class BinaryTree {
     }
 
     //remove um elemento existente na arvore o retorna
+    //Dhiego: Ele remove o valor, e depois atualiza a tabela,ai ele verifica se a direita e
+    // a esquerda são nulos caso sim ela retorna nulo,
+    //se não ele verifica se a direita é nulo, caso seja ele retorna a esqueda
+    //se não ele verifica a esquerda se for nulo, ele retorna a direita.
+    //caso ele tenha os dois nó ele retorna o valor que for maior 
     remove(value) {
         this.root = this.removeVisitor(this.root, value)
     }
@@ -135,6 +162,8 @@ class BinaryTree {
     }
 
     //exibe a altura da arvore
+    //Dhiego: primeiro ele verifica se o nó não tem nada, se ele não tiver ele retorna -1
+    //se não ele  verifica a altura esquerda e depois da direita.
     height() {
         return this.heightVisitor(this.root)
     }
@@ -148,6 +177,8 @@ class BinaryTree {
     }
 
     // informa quantos nós existem na arvore
+    //Dhiego: Ele verifica primeiro se tem nó caso não tenha ele retorna 0, caso tenha
+    //ele verifica os nós a direita e esquerda e soma mais 1.
     size() {
         return this.sizeVisitor(this.root)
     }
